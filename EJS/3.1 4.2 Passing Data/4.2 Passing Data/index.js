8f54d3bd0efd7d04e84d8e51ae4f944405dc2683
MD Jaliz Mahamud Mridul <mahmudmridul2003@gmail.com>
2024-11-16 20:36:38 +0600

    Passing Data EJS

--- EJS/3.1 4.2 Passing Data/4.2 Passing Data/index.js
+++ EJS/3.1 4.2 Passing Data/4.2 Passing Data/index.js
@@ -1,14 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { name } from "ejs";
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const noOfLetters = req.body["fName"].length + req.body["lName"].length;
+  res.render("index.ejs",{noL : noOfLetters});
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
